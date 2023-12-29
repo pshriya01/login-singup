95200f7525b6c07e2262284ffcc4d7626a4288d3
pshriya01 <shriya.shriya16@gmail.com>
2023-12-29 12:26:23 +0530

    expiration added to token

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# login-singup
+# Login-Singup Assingment
+
+This assingment include a login and signup page which can be useful for any type website
+
+# Frontend Deployed Link
+# [Live](https://frontend-two-sandy.vercel.app/)
+
+# Backend Deployed Link
+# [Live](https://brand-wick-g0pa.onrender.com)
+
+## TechStack used
+React.js | Node.js | mongoDB | express.js | Chakra UI
 
--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -3,6 +3,7 @@ const { UserModal } = require('../Models/UserModal')
 require('dotenv').config()
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const { BlacklistModel } = require('../Models/BlackListModal')
 const saltRounds = parseInt(process.env.saltrounds) || 10;
 const userRouter = express.Router()
 
@@ -12,13 +13,13 @@ userRouter.post("/signup", async (req,res) =>{
      const {name,username,email,password,phone} = req.body
      
         if(!name || !username || !email || !password || !phone){
-           res.status(400).send({message:"All Input fields are required!"})
+           res.status(200).send({message:"All Input fields are required!"})
         }
         else{
             const existingUser = await UserModal.findOne({email})
             
             if(existingUser){
-                res.status(409).send({message:"User with this Email Already Exists!"})
+                res.status(200).send({message:"User with this Email Already Exists!"})
             }
             else{
                 bcrypt.hash(password, saltRounds, async function(err, hash) {
@@ -45,19 +46,19 @@ userRouter.post("/signin", async (req,res) =>{
   try{
     const {email,password} = req.body
     if(!email || !password){
-        res.status(400).send({message:"All Input fields are required!"})
+        res.status(200).send({message:"All Input fields are required!"})
      }else{
         const user = await UserModal.findOne({email})
         if(!user){
-            res.status(404).send({message:"User Does Not Exist!"})
+            res.status(202).send({message:"User Does Not Exist!"})
         }else{
             const passwordMatch = await bcrypt.compare(password, user.password);
 
             if (passwordMatch) {
-                const token = jwt.sign({ user }, process.env.SecretKey);
+                const token = jwt.sign({ user }, process.env.SecretKey,{ expiresIn: 1*60 });
                 res.status(200).send({ message: "Login successful!", token });
             } else {
-                res.status(401).send({ message: "Incorrect password!" });
+                res.status(200).send({ message: "Incorrect password!" });
             }
         }
      }
@@ -68,6 +69,19 @@ userRouter.post("/signin", async (req,res) =>{
     res.status(500).send({ message: "Internal Server Error" });
 } 
 })
+
+
+userRouter.get("/logout", async (req, res) => {
+    const token = req.headers.authorization;
+    try {
+      if (token) {
+        await BlacklistModel.updateMany({}, { $push: { blacklist: [token] } });
+        res.status(200).json({ msg: "User has been logged out" });
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
 
 
 module.exports= {
